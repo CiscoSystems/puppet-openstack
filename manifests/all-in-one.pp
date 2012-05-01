00f576ed6654e041e5c26eaf84178575955c51ed
Soren Hansen <sorhanse@cisco.com>
2012-05-01 23:52:52 +0200

    Update all-in-one to use up-to-date modules

--- manifests/all-in-one.pp
+++ manifests/all-in-one.pp
@@ -42,7 +42,6 @@
 # 
 class openstack::all-in-one(
 	$mysql_root_password = 'UbQuaiwenn2',
-	$mysql_host = 'localhost',
 	$keystone_db_password = 'Peffyuc5',
 	$keystone_admin_token = 'TucMidIvtob2',
 	$glance_db_password = 'oidf73DFE',
@@ -60,7 +59,6 @@ class openstack::all-in-one(
 	$network_manager = 'nova.network.manager.FlatDHCPManager') {
 
 	# MySQL server
-	class { 'mysql::python': }
 	class { 'mysql::server':
 		config_hash => {
 			'root_password' => $mysql_root_password,
@@ -68,170 +66,137 @@ class openstack::all-in-one(
 	}
 
 	# Keystone Server
-	class { 'keystone::mysql':
+
+	# Sets up the Keystone DB
+	class { 'keystone::db::mysql':
 		password => $keystone_db_password,
 	}
+
+	# Configures Keystone to use the above DB
 	class { 'keystone::config::mysql':
 		password => $keystone_db_password,
 	}
+
 	class { 'keystone':
 		admin_token => $keystone_admin_token,
-		log_verbose  => true,
-		log_debug    => true,
+		log_verbose	=> true,
+		log_debug		=> true,
 		catalog_type => 'sql',
 	}
 
-	keystone_service { "keystone":
-		ensure => present,
-		type => "identity",
-		description => "OpenStack Identity Service"
+	# Set up an admin user
+	class { 'keystone::roles::admin':
+		password => $admin_password
 	}
 
-	keystone_endpoint { "keystone":
-		ensure => present,
-		public_url => "http://${public_ip}:5000/v2.0",
-		admin_url => "http://${admin_ip}:35357/v2.0",
-		internal_url => "http://${internal_ip}:5000/v2.0"
-	}
-
-	# Keystone tenant for services
-	keystone_tenant { "services":
-		ensure => present
-	}
-
-	keystone_role { "admin":
-		ensure => present
-	}
+	class { 'keystone::endpoint': }
 
 	# Glance
-	class { glance::db:
+	class { 'glance::keystone::auth':
+		password => $glance_service_password,
+	}
+
+	class { glance::db::mysql:
 		password => $glance_db_password
 	}
 
-	class { glance::registry:
+	class { glance::api:
+		auth_type => 'keystone',
+		auth_host => '127.0.0.1',
+		auth_port => '35357',
 		keystone_tenant => 'services',
 		keystone_user => 'glance',
 		keystone_password => $glance_service_password,
-		sql_connection => "mysql://glance:${glance_db_password}@${mysql_host}/glance",
-		require => [Class[glance::db], Keystone_user_role["glance@services"]]
 	}
 
-	class { glance::api:
+	class { glance::registry:
+		auth_type => 'keystone',
+		auth_host => '127.0.0.1',
+		auth_port => '35357',
 		keystone_tenant => 'services',
 		keystone_user => 'glance',
 		keystone_password => $glance_service_password,
-		require => Keystone_user_role["glance@services"]
+		sql_connection => "mysql://glance:${glance_db_password}@127.0.0.1/glance",
 	}
 
 	class { glance::backend::file: }
 
-	keystone_service { "glance":
-		ensure => present,
-		type => "image",
-		description => "OpenStack Image Service"
-	}
-
-	keystone_endpoint { "glance":
-		ensure => present,
-		public_url => "http://${public_ip}:9292/v1",
-		admin_url => "http://${admin_ip}:9292/v1",
-		internal_url => "http://${internal_ip}:9292/v1"
-	}
-
-	keystone_user { "glance":
-		password => $glance_service_password,
-		ensure => present
-	}
-
-	keystone_user_role { "glance@services":
-		roles => ["admin"]
-	}
-	
-
 	# Nova
+	class { 'nova::keystone::auth':
+		password => $nova_service_password,
+	}
+
 	class { nova::rabbitmq: }
 
-	class { nova::db:
+	class { nova::db::mysql:
 		password => $nova_db_password,
-		host => $nova_host,
+		host => 'localhost',
 	}
 
-	class { nova:
+	class { 'nova':
 		sql_connection => "mysql://nova:${nova_db_password}@${mysql_host}/nova",
-		image_service  => 'nova.image.glance.GlanceImageService',
-		glance_api_servers => '${glance_host}:9292',
+		image_service	=> 'nova.image.glance.GlanceImageService',
+		glance_api_servers => '127.0.0.1:9292',
 		network_manager => $network_manager,
-		admin_password => $nova_service_password,
 	}
 
-	class { nova::api:
+	class { 'nova::api':
 		enabled => true,
-		require => Keystone_user_role["nova@services"]
+		admin_password => $nova_service_password
 	}
 
-	class { nova::scheduler:
+	class { 'nova::scheduler':
 		enabled => true
 	}
 
-	class { nova::network:
+	class { 'nova::network':
 		enabled => true
 	}
 
-	class { nova::objectstore:
+	nova::manage::network { "nova-vm-net":
+		network => '192.168.188.0/24',
+		available_ips => '256',
+	}
+
+	class { 'nova::objectstore':
 		enabled => true
 	}
 
-	class { nova::compute:
-		enabled => true }
-
-	class { nova::compute::libvirt:
-		flat_network_bridge_ip => $bridge_ip,
-		flat_network_bridge_netmask => $bridge_netmask,
+	class { 'nova::volume':
+		enabled => true
 	}
 
-	keystone_service { "nova":
-		ensure => present,
-		type => "compute",
-		description => "OpenStack Compute Service"
+	class { 'nova::compute':
+		enabled											 => true,
+		vnc_enabled									 => true,
+		vncserver_proxyclient_address => '127.0.0.1',
+		vncproxy_host								 => $ipaddress,
 	}
 
-	keystone_endpoint { "nova":
-		ensure => present,
-		public_url => "http://${public_ip}:8774/v1.1/%(tenant_id)s",
-		admin_url => "http://${admin_ip}:8774/v1.1/%(tenant_id)s",
-		internal_url => "http://${internal_ip}:8774/v1.1/%(tenant_id)s"
+	class { 'nova::cert':
+		enabled => true
 	}
 
-	keystone_user { "nova":
-		ensure => present,
-		password => $nova_service_password
+	class { 'nova::consoleauth':
+		enabled => true
 	}
 
-	keystone_user_role { "nova@services":
-		roles => ["admin"]
+	class { 'nova::compute::libvirt':
+		libvirt_type		 => 'kvm',
+		vncserver_listen => '127.0.0.1',
+#		flat_network_bridge_ip => $bridge_ip,
+#		flat_network_bridge_netmask => $bridge_netmask,
 	}
 
+	class { 'memcached':
+		listen_ip => '127.0.0.1',
+	}
 
+	class { 'horizon': }
+
+	######## End Horizon #####
 	#
-	# Users
-	# 
-	keystone_tenant { "users":
-		ensure => present
-	}
-
-	keystone_user { "admin":
-		ensure => present,
-		password => $admin_password
-	}
-
-	keystone_role { "member":
-		ensure => present
-	}
-
-	keystone_user_role { "admin@users":
-		roles => ["member"]
-	}
-
+	# Clear out any settings from nova's config that we didn't put there
 	resources { 'nova_config':
 		purge => true,
 	}
