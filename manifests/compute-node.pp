00496f7ce84ba0b5f1f4815e3763112451052e3d
Soren Hansen <sorhanse@cisco.com>
2012-05-07 16:29:36 -0700

    Grant compute nodes access to MySQL
    Export a virtual host_access ressource from the compute-node. It gets
    picked up by nova::db::mysql and automagically grants the compute node
    access to MySQL.

--- manifests/compute-node.pp
+++ manifests/compute-node.pp
@@ -1,7 +1,7 @@
 class openstack::compute-node(
 	$mysql_root_password = 'UbQuaiwenn2',
 	$nova_db_password = 'gjD4sFJuds',
-        $ip = "${ipaddress_eth0}",
+	$ip = "${ipaddress_eth0}",
 	$bridge_ip = '192.168.188.1',
 	$bridge_netmask = '255.255.255.0',
 	$glance_api_servers = '127.0.0.1:9292',
@@ -28,5 +28,11 @@ class openstack::compute-node(
 	class { nova::compute::libvirt:
 		flat_network_bridge_ip => $bridge_ip,
 		flat_network_bridge_netmask => $bridge_netmask,
+	}
+
+	@@nova::db::mysql::host_access { $ip:
+		user => 'nova',
+		password => $nova_db_password,
+		database => 'nova'
 	}
 }
