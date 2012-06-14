390df4f0f5ce087d90490f1e35b4f44bd8327a3f
Soren Hansen <sorhanse@cisco.com>
2012-06-14 10:04:19 +0200

    Add an openstack::apt type for custom repos
    Adds a repository for custom built OpenStack packages. It will be
    configured before any package tagged "openstack".

---
+++ manifests/apt.pp
@@ -0,0 +1,26 @@
+#
+# This class is intended to serve as
+# a way of configuring a custom APT repository
+# for OpenStack packages.
+#
+# [location] URL (as you would put in sources.list)
+# [release] The release series to pull from (defaults to $lsbdistcodename)
+# [repos] Components to use from the repository (defaults to "main")
+# [key] ID of the signing key used in the repository
+# [key_source] A URL for the public key identified by key
+#
+define openstack::apt(
+  $location,
+  $release = $lsbdistcodename,
+  $repos   = 'main',
+  $key,
+  $key_source) {
+        apt::source { "openstack-${name}":
+                location          => $location,
+                release           => $release,
+                repos             => $repos,
+                key               => $key,
+                key_source        => $key_source,
+                include_src       => false,
+        } -> Package <| tag == "openstack" |>
+}
