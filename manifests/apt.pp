b1d25fe4ac0980282001374c5df9e86bfdec07b7
Soren Hansen <sorhanse@cisco.com>
2012-07-12 10:13:03 +0200

    Add simple support for adding PPA's

--- manifests/apt.pp
+++ manifests/apt.pp
@@ -3,7 +3,7 @@
 # a way of configuring a custom APT repository
 # for OpenStack packages.
 #
-# [location] URL (as you would put in sources.list)
+# [location] either URL (as you would put in sources.list) or "ppa:user/ppa_name"
 # [release] The release series to pull from (defaults to $lsbdistcodename)
 # [repos] Components to use from the repository (defaults to "main")
 # [key] ID of the signing key used in the repository
@@ -11,16 +11,23 @@
 #
 define openstack::apt(
   $location,
-  $release = $lsbdistcodename,
+  $release = $::lsbdistcodename,
   $repos   = 'main',
   $key,
   $key_source) {
-        apt::source { "openstack-${name}":
-                location          => $location,
-                release           => $release,
-                repos             => $repos,
-                key               => $key,
-                key_source        => $key_source,
-                include_src       => false,
-        } -> Package <| tag == "openstack" |>
+
+    if ($location =~ /^ppa:/) {
+      apt::ppa { "$location":
+        release => $release
+      } -> Package <| tag == "openstack" |>
+    } else {
+      apt::source { "openstack-${name}":
+        location          => $location,
+        release           => $release,
+        repos             => $repos,
+        key               => $key,
+        key_source        => $key_source,
+        include_src       => false,
+      } -> Package <| tag == "openstack" |>
+    }
 }
