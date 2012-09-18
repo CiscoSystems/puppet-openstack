afdc06e202e5fb4e7bfb05ea5be00abed6ed0fb2
Soren Hansen <sorhanse@cisco.com>
2012-09-18 13:14:11 +0200

    Leave it to apt module to create preferences.d

--- manifests/apt/pin.pp
+++ manifests/apt/pin.pp
@@ -1,10 +1,5 @@
 define openstack::apt::pin($pin_spec,
                            $pin_priority = 990) {
-    if (!defined(File["/etc/apt/preferences.d"])) {
-        file { "/etc/apt/preferences.d":
-            ensure => directory
-        }
-    }
     file { "/etc/apt/preferences.d/${name}.conf":
         content => template('openstack/apt-pinning.conf.erb'),
         ensure => "present",
